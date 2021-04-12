8dd31abac77505170320aa3fbf710d52896a6b65
Juan-calero <hello.juancalero@gmail.com>
2021-04-12 14:22:18 +0100

    3 clocks and responsiveness
    added all 3 clocks js and optimized for responsiveness and code reduction

--- js/main.js
+++ js/main.js
@@ -1,20 +1,40 @@
 const secPointer = document.getElementById("secPT");
 const minPointer = document.getElementById("minPT");
 const hourPointer = document.getElementById("hourPT");
+const hourLONPointer = document.getElementById('hourLON')
+const minLONPointer = document.getElementById('minLON');
+const secLONPointer = document.getElementById('secLON');
+const secNYPointer = document.getElementById('secNY');
+const minNYPointer = document.getElementById('minNY');
+const hourNYPointer = document.getElementById('hourNY');
 
-function setDatePT(){
+setInterval(setDate, 1000);
+
+function setDate(){
     const now = new Date();
 
     const sec = now.getSeconds();
-    const secDeg = (sec / 60) * 360 + 90;
-    secPointer.style.transform = `rotate(${secDeg}deg)`;
-
     const min = now.getMinutes();
-    const minDeg = (min / 60) * 360 + 90;
-    minPointer.style.transform = `rotate(${minDeg}deg)`;
-
     const hour = now.getHours();
+
+    const secDeg = (sec / 60) * 360 + 90;
+    const secrotate = `rotate(${secDeg}deg)`;
+    secPointer.style.transform = secrotate;
+    secNYPointer.style.transform = secrotate;
+    secLONPointer.style.transform = secrotate;
+
+
+    const minDeg = (min / 60) * 360 + 90;
+    const minrotate = `rotate(${minDeg}deg)`;
+
+    minPointer.style.transform = minrotate;
+    minNYPointer.style.transform = minrotate;
+    minLONPointer.style.transform = minrotate;
+
+    const hourNYDeg = ((hour - 4) % 12/ 12) * 360 + 90;
+    const hourLONDeg = ((hour + 1) % 12 / 12) * 360 + 90;
     const hourDeg = (hour / 12) * 360 + 90;
-    hourPointer.style.transform = `rotate(${hourDeg}deg)`
+    hourPointer.style.transform = `rotate(${hourDeg}deg)`;
+    hourNYPointer.style.transform = `rotate(${hourNYDeg}deg)`;
+    hourLONPointer.style.transform = `rotate(${hourLONDeg}deg)`;
 }
-setInterval(setDatePT, 1000);
